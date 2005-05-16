826ed197af909dff507259394ace9bbc6b2a34ef
Ivan Tubert-Brohman <ivan.tubert@gmail.com>
2005-05-16 05:08:38 +0000

    use find_bonds

--- t.pl
+++ t.pl
@@ -5,17 +5,9 @@ use warnings;
 use lib 'lib';
 use Chemistry::File::PDB;
 use Chemistry::File::VRML;
+use Chemistry::Bond::Find 'find_bonds';
 use diagnostics;
 
 my $mol = Chemistry::Mol->read('test.pdb');
-
-#my $pdb = $mol->file('xxx', format => 'vrml');
-##$pdb->add_mol($mol);
-#$pdb->open('>');
-#$pdb->write_mol($pdb->fh, $mol);
-#$pdb->centerAtoms();
-#$pdb->genBonds();
-#$pdb->setStyle('BallAndWire');
-#$pdb->setColor('byAtom');	# default
-
+find_bonds($mol, orders => 1);
 print $mol->print(format => 'vrml', centerAtoms => 1);
